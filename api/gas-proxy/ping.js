61a6c15e5b229f09dd8c67941ddbe68e0264bb90
tskrodenis1 <tom.skrodenis@gmail.com>
2025-08-14 07:37:38 +0300

    Update ping.js

--- api/gas-proxy/ping.js
+++ api/gas-proxy/ping.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
+  const agentKey = req.headers["x-agent-key"];
+  if (agentKey !== process.env.AGENT_KEY) {
+    return res.status(403).json({ status: "error", message: "Forbidden" });
+  }
+
   res.status(200).json({ status: "ok", message: "pong" });
 }
