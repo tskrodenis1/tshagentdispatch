c107796de30df32160bcec7de2cd280beec3327c
tskrodenis1 <tom.skrodenis@gmail.com>
2025-08-16 10:51:05 +0300

    Delete api/gas-proxy/ping.ts

--- api/gas-proxy/ping.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res
-    .status(200)
-    .setHeader('Content-Type', 'application/json')
-    .setHeader('Cache-Control', 'no-store')
-    .json({ ok: true, message: 'pong' });
-}
